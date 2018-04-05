b9e6ea12e757437c1aaf5b23604f81825bc15d31
Permanent Liu <permanent_liu@foxmail.com>
2018-04-05 21:10:31 +0800

    1

---
+++ App1/app.js
@@ -0,0 +1,26 @@
+//app.js
+App({
+  
+
+  getPhoneInfo(phone, info){
+    wx.request({
+      url: 'http://apis.juhe.cn/mobile/get?key=e4d4151244869fb453b467f4c9c46ecf', 
+      data: {
+        // "phone":phone
+        "phone": phone
+      },
+
+      success: function (res) {
+        // console.log(res.data)
+        info(res.data)
+      },
+
+      
+    })
+  },
+
+
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ App1/pages/index/index.js
@@ -0,0 +1,29 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+    data:{
+      inputValue: null,
+      returnedCompanyInfo: null,
+      returnedLocationInfo: null,
+  },
+
+  bindKeyInput: function(e){
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+
+  buttonOnClick: function(){
+    var tempInfo = this
+    console.log(this.data.inputValue)
+    app.getPhoneInfo(this.data.inputValue, function(data){
+      console.log(data)
+      tempInfo.setData({
+        returnedCompanyInfo:"中国" + data.result.company,
+        returnedLocationInfo: data.result.province + data.result.city,
+      })
+    });
+  },
+})
